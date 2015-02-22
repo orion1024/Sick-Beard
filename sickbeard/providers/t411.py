a254473bb5b69d2722792bec3a00585bf54fcc05
orion <orion@nosferatu>
2015-02-22 20:47:02 +0100

    Fix for transition from t411.me to t411.io

--- sickbeard/providers/t411.py
+++ sickbeard/providers/t411.py
@@ -34,11 +34,12 @@ class T411Provider(generic.TorrentProvider):
         generic.TorrentProvider.__init__(self, "T411")
 
         self.supportsBacklog = True
-        
+    
+    
         self.cj = cookielib.CookieJar()
         self.opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self.cj))
         
-        self.url = "http://www.t411.me"
+        self.url = "http://www.t411.io"
         
         self.login_done = False
         
@@ -155,11 +156,11 @@ class T411Provider(generic.TorrentProvider):
                     #title = link['title']
                     # Fortunately, we can still get it from the href. This is not really safe though as the href naming could very well change
                     # so it's best to check when the title gets back.
-                    title = link['href'].replace('//www.t411.me/torrents/', '')
+                    title = link['href'].replace('//www.t411.io/torrents/', '')
                     
                     
                     id = row.find_all('td')[2].find_all('a')[0]['href'][1:].replace('torrents/nfo/?id=','')
-                    downloadURL = ('http://www.t411.me/torrents/download/?id=%s' % id)
+                    downloadURL = ('http://www.t411.io/torrents/download/?id=%s' % id)
                     
                     logger.log(u"Search : HREF is %s,  Title is %s, download URL is %s" % (link['href'], title, downloadURL), logger.DEBUG) 
                     quality = Quality.nameQuality( title )
