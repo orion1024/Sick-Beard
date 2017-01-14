1020ff22a0005a79fa41445f8e645a26ed9f6427
Orion1024 <orion1024@users.noreply.github.com>
2017-01-15 00:38:57 +0100

    Switched to t411.li and various updates
    Updated search queries with * to have accurate results
    Updated terms ID for language, previous ones were no more valid
    Added User-Agent to pass CloudFlare check

--- sickbeard/providers/t411.py
+++ sickbeard/providers/t411.py
@@ -34,8 +34,7 @@ class T411Provider(generic.TorrentProvider):
         generic.TorrentProvider.__init__(self, "T411")
 
         self.supportsBacklog = True
-    
-    
+        
         self.cj = cookielib.CookieJar()
         self.opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self.cj))
         
@@ -43,28 +42,16 @@ class T411Provider(generic.TorrentProvider):
         
         self.login_done = False
         
-        self.failed_login_logged = False
-        self.successful_login_logged = False
-        
     def isEnabled(self):
         return sickbeard.T411
     
-    def getSearchParams(self, searchString, audio_lang, subcat=None, french=None):
-
-	if subcat==None:
-	       	if audio_lang == "en" and french==None:
-	            return urllib.urlencode( {'search': searchString, 'cat' : 210, 'submit' : 'Recherche' } ) + "&term%5B51%5D%5B%5D=1209&term%5B51%5D%5B%5D=1216"
-	        elif audio_lang == "fr" or french:
-        	    return urllib.urlencode( {'search': searchString, 'cat' : 210, 'submit' : 'Recherche' } ) + "&term%5B51%5D%5B%5D=1215&term%5B51%5D%5B%5D=1210"
-	        else:
-        	    return urllib.urlencode( {'search': searchString, 'cat' : 210, 'submit' : 'Recherche' } )
-	else:
-	       	if audio_lang == "en" and french==None:
-        	    return urllib.urlencode( {'search': searchString, 'cat' : 210, 'submit' : 'Recherche', 'subcat': subcat } ) + "&term%5B51%5D%5B%5D=1209&term%5B51%5D%5B%5D=1216"
-	        elif audio_lang == "fr" or french:
-        	    return urllib.urlencode( {'search': searchString, 'cat' : 210, 'submit' : 'Recherche', 'subcat': subcat } ) + "&term%5B51%5D%5B%5D=1215&term%5B51%5D%5B%5D=1210"
-		else:
-	            return urllib.urlencode( {'search': searchString, 'cat' : 210, 'submit' : 'Recherche', 'subcat': subcat } )
+    def getSearchParams(self, searchString, audio_lang, subcat, french=None):
+        if audio_lang == "en" and french==None:
+            return urllib.urlencode( {'search': searchString, 'cat' : 210, 'submit' : 'Recherche', 'subcat': subcat } ) + "&term%5B51%5D%5B%5D=1209&term%5B51%5D%5B%5D=1216"
+        elif audio_lang == "fr" or french:
+            return urllib.urlencode( {'search': searchString, 'cat' : 210, 'submit' : 'Recherche', 'subcat': subcat } ) + "&term%5B51%5D%5B%5D=1210&term%5B51%5D%5B%5D=1212&term%5B51%5D%5B%5D=1215"
+        else:
+            return urllib.urlencode( {'search': searchString, 'cat' : 210, 'submit' : 'Recherche', 'subcat': subcat } )
 
     def seasonValue(self, season):
         values = [968, 969, 970, 971, 972, 973, 974, 975, 976, 977, 978, 979, 980, 981, 982, 983, 984, 985, 986, 987, 988, 989, 990, 991, 994, 992, 993, 995, 996, 997]
@@ -98,97 +85,73 @@ class T411Provider(generic.TorrentProvider):
         results = []
         for showName in showNames:
             results.append( self.getSearchParams( "%s S%02dE%02d*" % ( showName, ep_obj.scene_season, ep_obj.scene_episode), ep_obj.show.audio_lang, 433, french ))
-            results.append( self.getSearchParams( "%s S%02dx%02d*" % ( showName, ep_obj.scene_season, ep_obj.scene_episode), ep_obj.show.audio_lang, 433, french ))
-            #results.append( self.getSearchParams( "%s S%02dx%02d" % ( showName, ep_obj.scene_season, ep_obj.scene_episode), ep_obj.show.audio_lang, 433, french ))
-            #results.append( self.getSearchParams( "%s S%02dE%02d" % ( showName, ep_obj.scene_season, ep_obj.scene_episode), ep_obj.show.audio_lang, 433, french ))
             if (int(ep_obj.scene_season) < 31 and int(ep_obj.scene_episode) < 61):
                 results.append( self.getSearchParams( showName, ep_obj.show.audio_lang, 433, french)+ "&" + urllib.urlencode({'term[46][]': self.episodeValue(ep_obj.scene_episode), 'term[45][]': self.seasonValue(ep_obj.scene_season)}))
             #results.append( self.getSearchParams( "%s %dx%d" % ( showName, ep_obj.season, ep_obj.episode ), ep_obj.show.audio_lang , 433 )) MAY RETURN 1x12 WHEN SEARCHING 1x1
-            results.append( self.getSearchParams( "%s %dx%02d" % ( showName, ep_obj.scene_season, ep_obj.scene_episode ), ep_obj.show.audio_lang, 433, french ))
+            results.append( self.getSearchParams( "%s %dx%02d*" % ( showName, ep_obj.scene_season, ep_obj.scene_episode ), ep_obj.show.audio_lang, 433, french ))
             results.append( self.getSearchParams( "%s S%02dE%02d*" % ( showName, ep_obj.scene_season, ep_obj.scene_episode), ep_obj.show.audio_lang, 637, french ))
-            #results.append( self.getSearchParams( "%s S%02dE%02d" % ( showName, ep_obj.scene_season, ep_obj.scene_episode), ep_obj.show.audio_lang, 637, french ))
             if (int(ep_obj.scene_season) < 31 and int(ep_obj.scene_episode) < 61):
                 results.append( self.getSearchParams( showName, ep_obj.show.audio_lang, 637, french)+ "&" + urllib.urlencode({'term[46][]': self.episodeValue(ep_obj.scene_episode), 'term[45][]': self.seasonValue(ep_obj.scene_season)}))
             #results.append( self.getSearchParams( "%s %dx%d" % ( showName, ep_obj.season, ep_obj.episode ), ep_obj.show.audio_lang, 637 ))
-            results.append( self.getSearchParams( "%s %dx%02d" % ( showName, ep_obj.scene_season, ep_obj.scene_episode ), ep_obj.show.audio_lang, 637, french ))
+            results.append( self.getSearchParams( "%s %dx%02d*" % ( showName, ep_obj.scene_season, ep_obj.scene_episode ), ep_obj.show.audio_lang, 637, french ))
             results.append( self.getSearchParams( "%s S%02dE%02d*" % ( showName, ep_obj.scene_season, ep_obj.scene_episode), ep_obj.show.audio_lang, 634, french))
-            #results.append( self.getSearchParams( "%s S%02dE%02d" % ( showName, ep_obj.scene_season, ep_obj.scene_episode), ep_obj.show.audio_lang, 634, french))
             #results.append( self.getSearchParams( "%s %dx%d" % ( showName, ep_obj.season, ep_obj.episode ), ep_obj.show.audio_lang, 634 ))
-            results.append( self.getSearchParams( "%s %dx%02d" % ( showName, ep_obj.scene_season, ep_obj.scene_episode ), ep_obj.show.audio_lang, 634, french ))
-
+            results.append( self.getSearchParams( "%s %dx%02d*" % ( showName, ep_obj.scene_season, ep_obj.scene_episode ), ep_obj.show.audio_lang, 634, french ))
         return results
     
     def _get_title_and_url(self, item):
         return (item.title, item.url)
     
+
     def getQuality(self, item):
         return item.getQuality()
     
     def _doLogin(self, login, password):
-       
+
+        #data = urllib.urlencode({'login': login, 'password' : password, 'submit' : 'Connexion', 'remember': 1, 'url' : '/'})
         data = urllib.urlencode({'login': login, 'password' : password, 'remember': 1, 'url' : '/'})
-        
-        req = urllib2.Request(self.url + '/users/auth/', data, {'X-Requested-With' : 'XMLHttpRequest'} )
-        
-        
-        r = self.opener.open(req)
-        
-        for index, cookie in enumerate(self.cj):
-            if (cookie.name == "authKey" and cookie.value != "null" ): self.login_done = True
-                                
-        if not self.login_done and not self.failed_login_logged:
-            logger.log(u"Unable to login to T411. Please check username and password.", logger.WARNING) 
-            self.failed_login_logged = True
-        
-        if self.login_done and not self.successful_login_logged:
-            logger.log(u"Login to T411 successful", logger.MESSAGE) 
-            self.successful_login_logged = True
+        #self.opener.open(self.url + '/users/auth/', data)
 
-
+        req = urllib2.Request(self.url + '/users/auth/', data, 
+                                 {'Connection' : 'keep-alive', 'DNT' : '1', 'Referer' : self.url+'/' ,'X-Requested-With' : 'XMLHttpRequest','User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'})
+        self.opener.open(req)
     
     def _doSearch(self, searchString, show=None, season=None, french=None):
         
         if not self.login_done:
             self._doLogin( sickbeard.T411_USERNAME, sickbeard.T411_PASSWORD )
-        
-        
+
         results = []
+        searchUrl = self.url + '/torrents/search/?' + searchString.replace('!','')
+        logger.log(u"Search string: " + searchUrl, logger.DEBUG)
         
-        if self.login_done:
-            
-            searchUrl = self.url + '/torrents/search/?' + searchString.replace('!','')
-            logger.log(u"Search string: " + searchUrl, logger.DEBUG)
-            
-            r = self.opener.open( searchUrl )
-            soup = BeautifulSoup( r, "html.parser" )
-            resultsTable = soup.find("table", { "class" : "results" })
-            if resultsTable:
-                rows = resultsTable.find("tbody").findAll("tr")
-        
-                for row in rows:
-                    link = row.find("a", title=True)
-                    # For now, the title does not contain the full name anymore. Maybe a bug from T411
-                    #title = link['title']
-                    # Fortunately, we can still get it from the href. This is not really safe though as the href naming could very well change
-                    # so it's best to check when the title gets back.
-                    title = link['href'].replace('//www.t411.li/torrents/', '')
-                    
-                    
-                    id = row.find_all('td')[2].find_all('a')[0]['href'][1:].replace('torrents/nfo/?id=','')
-                    downloadURL = ('https://www.t411.li/torrents/download/?id=%s' % id)
-                    
-                    logger.log(u"Search : HREF is %s,  Title is %s, download URL is %s" % (link['href'], title, downloadURL), logger.DEBUG) 
-                    quality = Quality.nameQuality( title )
-                    if quality==Quality.UNKNOWN and title:
-                        if '720p' not in title.lower() and '1080p' not in title.lower():
-                            quality=Quality.SDTV
-                    if show and french==None:
-                        results.append( T411SearchResult( self.opener, title, downloadURL, quality, str(show.audio_lang) ) )
-                    elif show and french:
-                        results.append( T411SearchResult( self.opener, title, downloadURL, quality, 'fr' ) )
-                    else:
-                        results.append( T411SearchResult( self.opener, title, downloadURL, quality ) )
-                    
+        req = urllib2.Request(searchUrl)
+        req.add_unredirected_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0')
+
+        r = self.opener.open( req )
+        #r = self.opener.open( searchUrl )
+        soup = BeautifulSoup( r, "html.parser" )
+        resultsTable = soup.find("table", { "class" : "results" })
+        if resultsTable:
+            rows = resultsTable.find("tbody").findAll("tr")
+    
+            for row in rows:
+                link = row.find("a", title=True)
+                title = link['title']
+                id = row.find_all('td')[2].find_all('a')[0]['href'][1:].replace('torrents/nfo/?id=','')
+                downloadURL = ('https://www.t411.li/torrents/download/?id=%s' % id)
+                
+                quality = Quality.nameQuality( title )
+                if quality==Quality.UNKNOWN and title:
+                    if '720p' not in title.lower() and '1080p' not in title.lower():
+                        quality=Quality.SDTV
+                if show and french==None:
+                    results.append( T411SearchResult( self.opener, link['title'], downloadURL, quality, str(show.audio_lang) ) )
+                elif show and french:
+                    results.append( T411SearchResult( self.opener, link['title'], downloadURL, quality, 'fr' ) )
+                else:
+                    results.append( T411SearchResult( self.opener, link['title'], downloadURL, quality ) )
+                
         return results
     
     def getResult(self, episodes):
@@ -210,7 +173,9 @@ class T411SearchResult:
         self.audio_langs=audio_langs
         
     def getNZB(self):
-        return self.opener.open( self.url , 'wb').read()
+        req = urllib2.Request(self.url)
+        req.add_unredirected_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0')
+        return self.opener.open(req).read()
 
     def getQuality(self):
         return self.quality
